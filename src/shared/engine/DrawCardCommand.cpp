7e1cda5094269d452721da782d2cc0fb02e153ff
Clovis Pressard <pcetudeing@gmail.com>
2025-12-05 08:20:46 +0100

    Update of the DrawCardCommand

--- src/shared/engine/DrawCardCommand.cpp
+++ src/shared/engine/DrawCardCommand.cpp
@@ -16,27 +16,8 @@ namespace engine
         if (cardType == state::DARK)
             engine.getCurrentPlayer().equipCards.push_back(engine.board->drawDark());
         if (cardType == state::HERMIT){
-            std::cout << "HERMIT card draw" << std::endl;
             engine.board->hermitPack.discard(engine.board->drawHermit());
-
-            //Test vector discard hermit
-            std::cout << "HERMIT card discard" << std::endl;
-            for(auto& card : engine.board->hermitPack.discardPile){
-                std::cout << card->name << " ";
-            }
         }
-            
-        
-        //Vérifie les carte équipé
-        std::cout << "DrawCardCommand" << std::endl;
-        std::cout << "Card of the player: ";
-        for (auto& card : engine.getCurrentPlayer().equipCards) {
-            std::cout << card->name << " ";
-            std::cout << card->type<< " ";
-        }
-        std::cout << std::endl;
-
-
         isDone = true;
     }
         
